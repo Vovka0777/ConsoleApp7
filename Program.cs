0849428314e9c249b2c1fa01c5f3cb5fe0a5e5af
AlekseiNime <mamagold1213@gmail.com>
2024-10-29 11:15:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.вывести в столбик 10 раз слово "Привет";
+
+            //int x=0;
+            //while (x<10)
+            //{
+            //    Console.WriteLine("Привет");
+            //    x++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("Привет");
+            //    x++;
+            //}
+            //while (x < 10);
+
+            //for (x=0;x<10;x++)
+            //{
+            //    Console.WriteLine("Привет");
+            //}
+
+            //2.вывести в столбик первые 10 натуральных чисел;
+
+            //int x = 1;
+            //while (x<=10)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+            //while (x <= 10);
+
+            //for (x=1;x<=10;x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //3.вывести в столбик первые 5 натуральных чисел в обратном порядке;
+
+            //int x = 5;
+            //while (x >= 1)
+            //{
+            //    Console.WriteLine(x);
+            //    x--;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x--;
+            //}
+            //while (x >= 1);
+
+            //for (x = 5; x >= 1; x--)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //4.вывести на экран целые числа из интервала от 1 до n;
+
+            //Console.Write("Введите кол-во - ");
+            //int num = int.Parse(Console.ReadLine());
+            //int x = 1;
+            //while (x <= num)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+            //while (x <= num);
+
+            //for (x = 1; x <= num; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //5.запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово и затем трижды каждое четное слово, между выводимыми словами вставить разделитель в виде трех символьных пробелов(“   ”).
+
+            //string[] wrds = new string[5];
+            //int x = 0;
+            //for (x=0;x<5;x++)
+            //{
+            //    Console.Write($"Введите {x} слово - ");
+            //    wrds[x] = Console.ReadLine();
+            //}
+            //for (x = 0; x < 5; x++)
+            //{
+            //    if (wrds[x].Length % 2 == 0)
+            //    {
+            //        Console.Write($"{wrds[x]}   {wrds[x]}   {wrds[x]}   ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write($"{wrds[x]}   {wrds[x]}   ");
+            //    }
+            //}
+            //Console.Read();
+        }
+    }
+}
